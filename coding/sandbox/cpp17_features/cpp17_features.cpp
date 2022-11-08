1b477b196d3c60a2038373ed3cf19a059c3c071f
Piotr Staniszewski <p.stanisz@gmail.com>
2022-11-08 21:53:44 +0100

    Sandbox: noexcept, lambda, constexpr

--- coding/sandbox/cpp17_features/cpp17_features.cpp
+++ coding/sandbox/cpp17_features/cpp17_features.cpp
@@ -3,28 +3,103 @@
 #include <iostream>
 #include <iomanip>
 
-template <typename T>
-void dump(const T c)
+namespace char_literals
 {
-    const uint8_t *data{reinterpret_cast<const uint8_t *>(&c)};
-
-    std::cout << std::hex << std::setfill('0');
-    for (auto idx{0U}; idx < sizeof(T); ++idx)
+    template <typename T>
+    void dump(const T c)
     {
-        std::cout << std::setw(2) << static_cast<unsigned>(data[idx]) << " ";
+        const uint8_t *data{reinterpret_cast<const uint8_t *>(&c)};
+
+        std::cout << std::hex << std::setfill('0');
+        for (auto idx{0U}; idx < sizeof(T); ++idx)
+        {
+            std::cout << std::setw(2) << static_cast<unsigned>(data[idx]) << " ";
+        }
+
+        std::cout << std::dec << std::endl;
     }
 
-    std::cout << std::endl;
+    template <typename T>
+    void print(std::string_view desc, const T c)
+    {
+        std::cout << desc << ":\t" << c << "\t";
+        dump(c);
+    }
+
+    void experiment();
 }
 
-template <typename T>
-void print(std::string_view desc, const T c)
+namespace noexcept_changed
 {
-    std::cout << desc << ":\t" << c << "\t";
-    dump(c);
+    int foo() { return 1; }
+    int bar() noexcept { return 10; }
+
+    void experiment();
 }
 
-void char_literals()
+namespace lambda_capture
+{
+    struct A
+    {
+        void print() const { std::cout << "print\n"; }
+        void method()
+        {
+            auto lambda = [=, *this]
+            { this->print(); };
+            lambda();
+        };
+    };
+
+    void experiment();
+}
+
+namespace constexpr_features
+{
+    template <typename T>
+    constexpr T increment_by_one(const T input)
+    {
+        // constexpr lambda example
+        auto is_incrementable_lambda = []() constexpr->bool
+        {
+            return std::is_arithmetic_v<T>;
+        };
+
+        // constexpr if example
+        if constexpr (is_incrementable_lambda())
+        {
+            return input + static_cast<T>(1);
+        };
+
+        return input;
+    }
+
+    constexpr int MY_INT = 10;
+    constexpr int MY_OUTPUT_INT = increment_by_one(MY_INT); // evaluated to 11
+
+    constexpr double MY_DOUBLE = 3.14;
+    constexpr double MY_OUTPUT_DOUBLE = increment_by_one(MY_DOUBLE); // evaluated to 4.14
+
+    constexpr const double *MY_PTR = &MY_DOUBLE;
+    constexpr const double *MY_OUTPUT_PTR = increment_by_one(MY_PTR); // unchanged and compiles
+}
+
+int main()
+{
+    std::cout << "C++17 features\n";
+
+    // Experiments with char literals
+    char_literals::experiment();
+
+    // noexcept as a part of type system
+    noexcept_changed::experiment();
+
+    // Lambda capture of *this
+    lambda_capture::experiment();
+
+    return EXIT_SUCCESS;
+}
+
+void char_literals::experiment()
 {
     // 'a' in UTF-8
     const char c1 = u8'a';
@@ -43,11 +118,18 @@ void char_literals()
     print("Wide", c4);
 }
 
-int main()
+void noexcept_changed::experiment()
 {
-    std::cout << "C++17 features\n";
+    // error: invalid conversion from ‘int (*)()’ to ‘int (*)() noexcept’
+    // int (*foo_ptr)() noexcept = foo;
+    // std::cout << "foo via functor: " << foo_ptr() << "\n";
 
-    char_literals();
+    int (*bar_ptr)() noexcept = bar;
+    std::cout << "bar via functor: " << bar_ptr() << "\n";
+}
 
-    return EXIT_SUCCESS;
+void lambda_capture::experiment()
+{
+    A obj;
+    obj.method();
 }
--- coding/sandbox/cpp17_features/readme.md
+++ coding/sandbox/cpp17_features/readme.md
@@ -1,4 +1,4 @@
-# C++17 most useful features
+# C++17 features
 
 Examples of **C++17** features.
 
@@ -18,4 +18,67 @@ Available since **C++11**, except of `u8'` added by **C++17**.
 
     // 'a' in wide character literal
     const wchar_t c4 = L'a';    // Wide:   61      61 00 00 00
+```
+
+## noexcept
+
+`noexcept` is now a part of type system
+
+```cpp
+    // error: invalid conversion from ‘int (*)()’ to ‘int (*)() noexcept’
+    // int (*foo_ptr)() noexcept = foo;
+    // std::cout << "foo via functor: " << foo_ptr() << "\n";
+
+    int (*bar_ptr)() noexcept = bar;
+    std::cout << "bar via functor: " << bar_ptr() << "\n";
+```
+
+## Lambda capture of `*this`
+
+Possible in **C++17** to capture `*this` by copy
+
+```cpp
+    struct A
+    {
+        void print() const { std::cout << "print\n"; }
+        void method()
+        {
+            auto lambda = [=, *this]
+            { this->print(); };
+            lambda();
+        };
+    };
+```
+
+## constexpr if and lambda
+
+`constexpr if` can be used, as well as `constexpr` lambdas
+
+```cpp
+    template <typename T>
+    constexpr T increment_by_one(const T input)
+    {
+        // constexpr lambda example
+        auto is_incrementable_lambda = []() constexpr->bool
+        {
+            return std::is_arithmetic_v<T>;
+        };
+
+        // constexpr if example
+        if constexpr (is_incrementable_lambda())
+        {
+            return input + static_cast<T>(1);
+        };
+
+        return input;
+    }
+
+    constexpr int MY_INT = 10;
+    constexpr int MY_OUTPUT_INT = increment_by_one(MY_INT); // evaluated to 11
+
+    constexpr double MY_DOUBLE = 3.14;
+    constexpr double MY_OUTPUT_DOUBLE = increment_by_one(MY_DOUBLE); // evaluated to 4.14
+
+    constexpr const double *MY_PTR = &MY_DOUBLE;
+    constexpr const double *MY_OUTPUT_PTR = increment_by_one(MY_PTR); // unchanged and compiles
 ```
