e3c2072305d4ce2f5d0523f26bc2cd416c74ae95
Piotr Staniszewski <p.stanisz@gmail.com>
2023-03-20 21:31:35 +0100

    is_palindrome added

--- coding/algorithms/largest_palindrome/src/palindrome_number.hpp
+++ coding/algorithms/largest_palindrome/src/palindrome_number.hpp
@@ -6,6 +6,13 @@ template <typename T>
 constexpr bool is_palindrome(T number)
 {
     static_assert(std::is_integral<T>::value, "Integral type required.");
+    const T orig_number = number;
 
-    return false;
+    T rev_number = 0;
+    while (number > 0)
+    {
+        rev_number = rev_number * 10 + number % 10;
+        number = number / 10;
+    }
+    return rev_number == orig_number;
 }
--- coding/algorithms/largest_palindrome/test/palindrome_number_test.cpp
+++ coding/algorithms/largest_palindrome/test/palindrome_number_test.cpp
@@ -4,5 +4,13 @@
 
 TEST(Palindrome_number, first)
 {
-    EXPECT_TRUE(true);
+    EXPECT_FALSE(is_palindrome(12));
+    EXPECT_FALSE(is_palindrome(123));
+    EXPECT_FALSE(is_palindrome(99198));
+    EXPECT_FALSE(is_palindrome(5512215));
+
+    EXPECT_TRUE(is_palindrome(1));
+    EXPECT_TRUE(is_palindrome(121));
+    EXPECT_TRUE(is_palindrome(9223229));
+    EXPECT_TRUE(is_palindrome(55122155));
 }
