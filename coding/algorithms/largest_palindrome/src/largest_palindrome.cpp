d5aa12f020b914fb17b5ee6eca9a8677bfb5dae4
Piotr Staniszewski <p.stanisz@gmail.com>
2023-03-21 21:10:22 +0100

    Adding google benchmark

--- coding/algorithms/largest_palindrome/src/largest_palindrome.cpp
+++ coding/algorithms/largest_palindrome/src/largest_palindrome.cpp
@@ -1,40 +1,51 @@
 // Copyright (c) 2023, Piotr Staniszewski
 
-#include <iostream>
-
 #include "palindrome_number.hpp"
+
+#include <benchmark/benchmark.h>
+
+#include <iostream>
+#include <tuple>
 
 constexpr uint64_t MAX_N_DIGIT_NUMBER = 99U;
 constexpr uint64_t MAX_PRODUCT_OF_N_DIGIT_NUMBER = MAX_N_DIGIT_NUMBER * MAX_N_DIGIT_NUMBER;
 
-uint64_t brute_force()
+auto brute_force()
 {
+    std::tuple<uint64_t, uint64_t, uint64_t> result = std::make_tuple(0U, 0U, 0U);
     uint64_t number1 = MAX_N_DIGIT_NUMBER;
-    
-    uint64_t largest_palindrome = 0U;
-    while (number1 > 0) {
+
+    while (number1 > 0)
+    {
         uint64_t number2 = MAX_N_DIGIT_NUMBER;
-        while (number2 > 0) {
+        while (number2 > 0)
+        {
             auto product = number1 * number2;
-            if (is_palindrome(product)) {
-                if (largest_palindrome < product) {
-                    largest_palindrome = product;
-                    break;
+            if (is_palindrome(product))
+            {
+                if (std::get<0>(result) < product)
+                {
+                    std::get<0>(result) = product;
+                    std::get<1>(result) = number1;
+                    std::get<2>(result) = number2;
                 }
             }
             --number2;
-        } 
+        }
         --number1;
     }
 
-    return largest_palindrome;
+    return result;
 }
 
-int main()
+static void benchmark_brute_force(benchmark::State &state)
 {
-    std::cout << "largest palindrome\n";
-
-    std::cout << "Calculated with brute force: " << brute_force() << std::endl;
-
-    return EXIT_SUCCESS;
+    for (auto _ : state)
+    {
+        std::tuple<uint64_t, uint64_t, uint64_t> result = brute_force();
+        std::cout << std::get<0>(result) << " = " << std::get<1>(result) << " * " << std::get<2>(result) << std::endl;
+    }
 }
+BENCHMARK(benchmark_brute_force);
+
+BENCHMARK_MAIN();
