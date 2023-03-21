7ff08c3df6395b3b684d6d9e9481e8336d154c99
Piotr Staniszewski <p.stanisz@gmail.com>
2023-03-21 07:20:39 +0100

    brute force implemented

--- coding/algorithms/largest_palindrome/src/largest_palindrome.cpp
+++ coding/algorithms/largest_palindrome/src/largest_palindrome.cpp
@@ -2,9 +2,39 @@
 
 #include <iostream>
 
+#include "palindrome_number.hpp"
+
+constexpr uint64_t MAX_N_DIGIT_NUMBER = 99U;
+constexpr uint64_t MAX_PRODUCT_OF_N_DIGIT_NUMBER = MAX_N_DIGIT_NUMBER * MAX_N_DIGIT_NUMBER;
+
+uint64_t brute_force()
+{
+    uint64_t number1 = MAX_N_DIGIT_NUMBER;
+    
+    uint64_t largest_palindrome = 0U;
+    while (number1 > 0) {
+        uint64_t number2 = MAX_N_DIGIT_NUMBER;
+        while (number2 > 0) {
+            auto product = number1 * number2;
+            if (is_palindrome(product)) {
+                if (largest_palindrome < product) {
+                    largest_palindrome = product;
+                    break;
+                }
+            }
+            --number2;
+        } 
+        --number1;
+    }
+
+    return largest_palindrome;
+}
+
 int main()
 {
     std::cout << "largest palindrome\n";
+
+    std::cout << "Calculated with brute force: " << brute_force() << std::endl;
 
     return EXIT_SUCCESS;
 }
