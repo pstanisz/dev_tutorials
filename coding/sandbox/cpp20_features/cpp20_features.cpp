3f035c26d291850bae711e94d104058261465d98
Piotr Staniszewski <p.stanisz@gmail.com>
2023-10-23 20:55:10 +0200

    cpp20_features empty

---
+++ coding/sandbox/cpp20_features/cpp20_features.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2023, Piotr Staniszewski
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "C++20 features\n";
+
+    return EXIT_SUCCESS;
+}
---
+++ coding/sandbox/cpp20_features/readme.md
@@ -0,0 +1,11 @@
+# C++20 features
+
+Examples of **C++20** features.
+
+## TODO
+
+TODO
+
+```cpp
+    //TODO
+```
