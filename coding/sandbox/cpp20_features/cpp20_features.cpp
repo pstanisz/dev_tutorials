a11a02bb3c5b49f0ff7c653282cdc3a364f087ee
Piotr Staniszewski <p.stanisz@gmail.com>
2023-10-23 21:31:56 +0200

    <=>, std::span

--- coding/sandbox/cpp20_features/cpp20_features.cpp
+++ coding/sandbox/cpp20_features/cpp20_features.cpp
@@ -1,10 +1,88 @@
 // Copyright (c) 2023, Piotr Staniszewski
 
 #include <iostream>
+#include <vector>
+#include <span>
+
+void print(std::span<int> numbers) {
+    bool first{true};
+    for (auto number : numbers) {
+        if (first) {
+            std::cout << number;
+            first = false;
+        } else {
+            std::cout << ", " << number;
+        }
+    }
+
+    std::cout << "\n";
+}
 
 int main()
 {
     std::cout << "C++20 features\n";
+
+    // Three-way comparison (spaceship operator)
+    {
+        int x = 10;
+        int y = 20;
+
+        auto res = x <=> y;
+
+        if (res < 0) {
+            std::cout << x << " is less than " << y << "\n";
+        } else if (res > 0) {
+            std::cout << x << " is greater than " << y << "\n";
+        } else if (res == 0) {
+            std::cout << x << " is equal to " << y << "\n";
+        } else {
+            std::cout << "unordered\n";
+        }
+    }
+
+    {
+        std::string_view x = "hello";
+        std::string_view y = "Hellno";
+
+        auto res = x <=> y;
+
+        if (res < 0) {
+            std::cout << x << " is less than " << y << "\n";
+        } else if (res > 0) {
+            std::cout << x << " is greater than " << y << "\n";
+        } else if (res == 0) {
+            std::cout << x << " is equal to " << y << "\n";
+        } else {
+            std::cout << "unordered\n";
+        }
+    }
+
+    {
+        std::vector<int> x = {1,2,3,4,5};
+        std::vector<int> y = {1,2,3,5,5};
+
+        auto res = x <=> y;
+
+        if (res < 0) {
+            std::cout << "less" << "\n";
+        } else if (res > 0) {
+            std::cout << "greater" << "\n";
+        } else if (res == 0) {
+            std::cout << "equal" << "\n";
+        } else {
+            std::cout << "unordered\n";
+        }
+    }
+
+    // span
+    int numbers_a[] = {-1,0,1};
+    print(numbers_a);
+
+    std::vector<int> numbers_b {1,2,3};
+    print(numbers_b);
+
+    std::array<int, 4> numbers_c {1,2,3,4};
+    print(numbers_c);
 
     return EXIT_SUCCESS;
 }
