ae32ff0078ec3eb07e711e6b443b1aeb1210da6f
Piotr Staniszewski <p.stanisz@gmail.com>
2023-11-07 11:39:24 +0100

    function templates playground

--- coding/sandbox/meta/main.cpp
+++ coding/sandbox/meta/main.cpp
@@ -63,9 +63,49 @@ void print_if_streamable(T t)
     std::cout << t << "\n";
 }
 
+// Function template - no limitation
+template <typename T>
+void print_number_a(T number)
+{
+    std::cout << number << "\n";
+}
+
+// Function template - limited to integral types by enable_if
+template <typename T, std::enable_if_t<std::is_integral<T>::value, bool> = true>
+void print_number_b(T number)
+{
+    std::cout << number << "\n";
+}
+
+// Function template - limited to floating types by static_assert
+template <typename T>
+void print_number_c(T number)
+{
+    static_assert(std::is_floating_point<T>::value == true);
+    std::cout << number << "\n";
+}
+
+// Function template - limited to integral types by requires
+template <typename T>
+requires std::is_integral_v<T>
+void print_number_d(T number)
+{
+    std::cout << number << "\n";
+}
+
+// Function template - limited to floting types by concept + requires
+template <typename T>
+concept Floating_point = std::is_floating_point_v<T>;
+
+template <Floating_point T>
+void print_number_e(T number)
+{
+    std::cout << number << "\n";
+}
+
 int main(int /*argc*/, char * /*argv*/[])
 {
-    std::cout << "templates" << std::endl;
+    std::cout << "meta" << std::endl;
 
     int i = 0;
     float j = 5.0;
@@ -81,7 +121,7 @@ int main(int /*argc*/, char * /*argv*/[])
 
     // print_if_convertible(i, s);
     print_if_convertible(i, j);
-    //print_if_convertible(small, big);
+    // print_if_convertible(small, big);
 
     std::cout << small << "\n";
     // std::cout << big << "\n";
@@ -92,6 +132,31 @@ int main(int /*argc*/, char * /*argv*/[])
     print_if_streamable(small);
     // print_if_streamable(big);
     print_if_streamable(s);
+
+    print_number_a(1);
+    print_number_a(2UL);
+    print_number_a(3.0);
+    print_number_a(4.5f);
+
+    print_number_b(1);
+    print_number_b(2UL);
+    // print_number_b(3.0);
+    // print_number_b(4.5f);
+
+    // print_number_c(1);
+    // print_number_c(2UL);
+    print_number_c(3.0);
+    print_number_c(4.5f);
+
+    print_number_d(1);
+    print_number_d(2UL);
+    // print_number_d(3.0);
+    // print_number_d(4.5f);
+
+    // print_number_e(1);
+    // print_number_e(2UL);
+    print_number_e(3.0);
+    print_number_e(4.5f);
 
     return 0;
 }
