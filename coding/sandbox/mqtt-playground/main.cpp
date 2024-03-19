76276a172978c17220099bbedb0670d1b653f524
Piotr Staniszewski <p.stanisz@gmail.com>
2024-03-19 21:16:11 +0100

    mqtt wip

--- coding/sandbox/mqtt-playground/main.cpp
+++ coding/sandbox/mqtt-playground/main.cpp
@@ -1,66 +1,93 @@
 #include <iostream>
 #include <thread>
+#include <format>
 
 #include <mqtt/client.h>
 
-const std::string TOPIC = "topic";
-const std::string PAYLOAD = "hello!";
+const std::string TOPIC = "Status";
 
 int main(int /*argc*/, char * /*argv*/[])
 {
-    std::cout << "mqtt-playground\n";
+  std::cout << "mqtt-playground\n";
 
-    const std::string address = "127.0.0.1:1234";
-    const std::string producer_id = "producer";
-    const std::string consumer_id = "consumer";
+  const std::string address = "[::1]:1883";
+  const std::string producer_id = "producer";
+  const std::string consumer_id = "consumer";
 
-    mqtt::client consumer(address, consumer_id, mqtt::create_options(mqtt::MQTTPP_V3_CODE));
+  mqtt::client consumer(address, consumer_id);
 
-    mqtt::connect_options con_opts;
-    con_opts.set_keep_alive_interval(100);
-    con_opts.set_clean_session(true);
+  auto con_opts = mqtt::connect_options_builder().clean_session(false).finalize();
 
-    mqtt::client producer(address, producer_id, mqtt::create_options(mqtt::MQTTPP_V3_CODE));
+  mqtt::client producer(address, producer_id,
+                        mqtt::create_options(mqtt::MQTTPP_V3_CODE));
 
-    std::jthread producer_thread([&producer, &con_opts]()
-                                 {
+  std::jthread producer_thread([&producer, &con_opts]()
+                               {
     try {
+      std::cout << "Producer thread started\n";
+
       producer.connect(con_opts);
 
-      // auto msg = mqtt::make_message(TOPIC, PAYLOAD);
-      // msg->set_qos(mqtt::GRANTED_QOS_0);
+      for (size_t idx = 0; idx < 10; ++idx) {
 
-      // producer.publish(msg);
+        auto payload = std::format("payload {}", idx);
+        auto msg = mqtt::make_message(TOPIC, payload);
+        msg->set_qos(mqtt::GRANTED_QOS_0);
+
+        std::cout << "Producer\nTopic:\t\t" << TOPIC << "\nMessage:\t" << payload << "\n";
+
+        producer.publish(msg);
+
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+      }
+
+      auto msg = mqtt::make_message(TOPIC, "exit");
+      msg->set_qos(mqtt::GRANTED_QOS_0);
+      std::cout << "Producer\nTopic:\t\t" << TOPIC << "\nMessage:\t" << "exit" << "\n";
+      producer.publish(msg);
 
       producer.disconnect();
+      std::cout << "Producer thread ended\n";
+
     } catch (const mqtt::exception &e) {
       std::cerr << "MQTT exception caught: " << e.what() << std::endl;
     } catch (const std::exception &e) {
       std::cerr << "Standard exception caught: " << e.what() << std::endl;
     } });
 
-    std::jthread consumer_thread([&consumer, &con_opts]()
-                                 {
+  std::jthread consumer_thread([&consumer, &con_opts]()
+                               {
     try {
-      consumer.connect(con_opts);
+      std::cout << "Consumer thread started\n";
 
+      consumer.connect(con_opts);
       consumer.subscribe(TOPIC);
       consumer.start_consuming();
 
-      // auto msg = mqtt::make_message(TOPIC, PAYLOAD);
-      // msg->set_qos(mqtt::GRANTED_QOS_0);
+      while (true) {
+        auto msg = consumer.consume_message();
+        if (!msg) {
+          break;
+        }
 
-      // consumer.publish(msg);
+        std::cout << "Consumer\nTopic:\t\t" << msg->get_topic() << "\nMessage:\t" << msg->to_string() << "\n";
+
+        if (msg->to_string() == "exit") {
+          break;
+        }
+      }
 
       consumer.disconnect();
+      std::cout << "Consumer thread ended\n";
+
     } catch (const mqtt::exception &e) {
       std::cerr << "MQTT exception caught: " << e.what() << std::endl;
     } catch (const std::exception &e) {
       std::cerr << "Standard exception caught: " << e.what() << std::endl;
     } });
 
-    consumer_thread.join();
-    producer_thread.join();
+  consumer_thread.join();
+  producer_thread.join();
 
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
