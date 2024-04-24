421864b6342cb86facde48c4f752b0276c961e6f
Piotr Staniszewski <p.stanisz@gmail.com>
2024-04-24 21:36:48 +0200

    Dummy Overwritting_allocator

--- coding/sandbox/allocators/allocators.cpp
+++ coding/sandbox/allocators/allocators.cpp
@@ -65,15 +65,92 @@ namespace simple
 
 }
 
+namespace medium
+{
+
+    /**
+     * @brief Custom allocator that allocates memory from a fixed-size area.
+     *
+     * This allocator provides a fixed-size memory area from which memory is allocated and deallocated.
+     * It ensures that the allocated memory does not exceed a predefined maximum size.
+     *
+     * @tparam T The type of elements to allocate memory for.
+     */
+    template <typename T>
+    class Overwritting_allocator
+    {
+    public:
+        using value_type = T;
+        using size_type = std::size_t;
+        using difference_type = std::ptrdiff_t;
+
+        static constexpr const size_type MAX = 1024;
+
+        Overwritting_allocator() noexcept
+        {
+            std::cout << "Overwritting_allocator ctor\n";
+        }
+
+        template <typename U>
+        Overwritting_allocator(const Overwritting_allocator<U> &other) noexcept
+        {
+            std::cout << "Overwritting_allocator copy ctor\n";
+        }
+
+        ~Overwritting_allocator() noexcept
+        {
+            std::cout << "Overwritting_allocator dtor\n";
+        }
+
+        [[nodiscard]] constexpr T *allocate(size_type n)
+        {
+            std::cout << "Allocate memory for " << n << " element(s)\n";
+            if (n > MAX)
+            {
+                throw std::bad_alloc();
+            }
+            T *ptr = &m_fixed_memory_area[0];
+
+            std::cout << "Memory allocated at: " << std::hex << &m_fixed_memory_area[0] << std::dec << "\n";
+
+            return ptr;
+        }
+
+        constexpr void deallocate(T *, size_type) noexcept
+        {
+            std::cout << "Deallocate memory\n";
+
+            std::cout << "Memory deallocated at: " << std::hex << &m_fixed_memory_area[0] << std::dec << "\n";
+        }
+
+    private:
+        T m_fixed_memory_area[MAX];
+    };
+
+}
+
 int main()
 {
     std::cout << "allocators\n";
 
-    std::vector<int, simple::Counting_allocator<int>> vec;
+    std::vector<int, simple::Counting_allocator<int>> vec1;
     for (int i = 0; i < 10; ++i)
     {
-        vec.push_back(i);
+        vec1.push_back(i);
     }
+
+    std::vector<int, medium::Overwritting_allocator<int>> vec2;
+    for (int i = 10; i < 20; ++i)
+    {
+        vec2.push_back(i);
+    }
+
+    for (auto item : vec2)
+    {
+        std::cout << item << ", ";
+    }
+
+    std::cout << "\n";
 
     return EXIT_SUCCESS;
 }
