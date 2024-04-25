a2829eb8cd931d0caa1174757cc1cdaa9d9041b0
Piotr Staniszewski <p.stanisz@gmail.com>
2024-04-25 21:34:04 +0200

    Safe_allocator started

--- coding/sandbox/allocators/allocators.cpp
+++ coding/sandbox/allocators/allocators.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <limits>
 #include <vector>
+#include <algorithm>
 
 namespace simple
 {
@@ -137,6 +138,121 @@ namespace medium
 
 }
 
+namespace complex
+{
+
+    template <typename T>
+    class Safe_allocator
+    {
+    public:
+        using value_type = T;
+        using size_type = std::size_t;
+        using difference_type = std::ptrdiff_t;
+
+        static constexpr const size_type MAX = 1024;
+
+        Safe_allocator() noexcept : m_allocated_sections{}, m_free_sections{{&m_memory[0], MAX}}
+        {
+            std::cout << "Safe_allocator ctor\n";
+
+            std::cout << "Allocated sections: " << m_allocated_sections.size() << "\n";
+            std::cout << "Free sections: " << m_free_sections.size() << "\n";
+        }
+
+        template <typename U>
+        Safe_allocator(const Safe_allocator<U> &other) noexcept : m_allocated_sections{other.m_allocated_sections}, m_free_sections{other.m_free_sections}
+        {
+            std::cout << "Safe_allocator copy ctor\n";
+
+            std::cout << "Allocated sections: " << m_allocated_sections.size() << "\n";
+            std::cout << "Free sections: " << m_free_sections.size() << "\n";
+        }
+
+        ~Safe_allocator() noexcept
+        {
+            std::cout << "Safe_allocator dtor\n";
+
+            std::cout << "Allocated sections: " << m_allocated_sections.size() << "\n";
+            std::cout << "Free sections: " << m_free_sections.size() << "\n";
+        }
+
+        [[nodiscard]] constexpr T *allocate(size_type n)
+        {
+            std::cout << "Allocate memory for " << n << " element(s)\n";
+
+            if (m_free_sections.empty())
+            {
+                throw std::bad_alloc();
+            }
+
+            // TODO: find best matching section (of exact requested size)
+            auto size_equal_or_more = [&n](const Section &s)
+            { return (n <= s.m_length); };
+            auto it = std::find_if(std::begin(m_free_sections), std::end(m_free_sections), size_equal_or_more);
+            if (it == std::end(m_free_sections))
+            {
+                throw std::bad_alloc();
+            }
+
+            auto free_section = *it;
+
+            auto allocated_section = Section(free_section.m_begin, n);
+            m_allocated_sections.push_back(allocated_section);
+
+            if (free_section.m_length > n)
+            {
+                free_section.m_begin += n;
+                free_section.m_length -= n;
+            }
+            else
+            {
+                m_free_sections.erase(it);
+            }
+
+            std::cout << "Allocated sections: " << m_allocated_sections.size() << "\n";
+            std::cout << "Free sections: " << m_free_sections.size() << "\n";
+
+            return allocated_section.m_begin;
+        }
+
+        constexpr void deallocate(T *ptr, size_type n) noexcept
+        {
+            std::cout << "Deallocate memory\n";
+
+            auto section_found = [ptr, &n](const Section &s)
+            { return (ptr == s.m_begin && n == s.m_length); };
+            auto it = std::find_if(std::begin(m_allocated_sections), std::end(m_allocated_sections), section_found);
+            if (it == std::end(m_allocated_sections))
+            {
+                std::cerr << "Invalid deallocation requested?\n";
+            }
+
+            m_free_sections.push_back(*it);
+            m_allocated_sections.erase(it);
+
+            // TODO: merge free neighbouring sections
+
+            std::cout << "Allocated sections: " << m_allocated_sections.size() << "\n";
+            std::cout << "Free sections: " << m_free_sections.size() << "\n";
+        }
+
+    private:
+        struct Section
+        {
+            Section(T *begin, size_type n) : m_begin{begin}, m_length{n} {}
+
+            T *m_begin;
+            size_type m_length;
+        };
+
+        T m_memory[MAX];
+
+        std::vector<Section> m_allocated_sections;
+        std::vector<Section> m_free_sections;
+    };
+
+}
+
 int main()
 {
     std::cout << "allocators\n";
@@ -159,6 +275,12 @@ int main()
     }
 
     std::cout << "\n";
+
+    std::vector<int, complex::Safe_allocator<int>> vec3;
+    for (int i = 20; i < 30; ++i)
+    {
+        vec3.push_back(i);
+    }
 
     return EXIT_SUCCESS;
 }
